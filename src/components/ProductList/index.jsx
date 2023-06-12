5e32856558ed0373d0a06aaa0c8d244e05b07ef6
Son281120 <changngoc10x28@gmail.com>
2023-06-12 08:17:28 +0700

    add react pagination

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -1,10 +1,21 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import ReactPaginate from 'react-paginate'
 import ProductItem from './ProductItem'
 
-
 import './ProductList.scss'
+
 const ProductList = ({ display, status, error }) => {
 
+    const [currentPage, setCurrentPage] = useState(0);
+    const [perPage] = useState(12);
+
+    const handlePageChange = ({ selected }) => {
+        setCurrentPage(selected);
+    };
+
+    const offset = currentPage * perPage;
+    const paginatedProducts = display.slice(offset, offset + perPage);
 
     return (
         <div className="product col-xs-9 col-sm-12 col-md-12 col-lg-9 col-hg-12">
@@ -22,7 +33,7 @@ const ProductList = ({ display, status, error }) => {
             </div>
             <div className="row product__list">
                 {
-                    display.map(item => {
+                    paginatedProducts.map(item => {
                         return <ProductItem
                             key={item.id}
                             id={item.id}
@@ -33,6 +44,19 @@ const ProductList = ({ display, status, error }) => {
                         />
                     })
                 }
+                <ReactPaginate
+                    previousLabel={'Previous'}
+                    nextLabel={'Next'}
+                    breakLabel={'...'}
+                    breakClassName={'break-me'}
+                    pageCount={Math.ceil(display.length / perPage)}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={5}
+                    onPageChange={handlePageChange}
+                    containerClassName={'pagination'}
+                    subContainerClassName={'pages pagination'}
+                    activeClassName={'active'}
+                />
             </div>
         </div>
     )
